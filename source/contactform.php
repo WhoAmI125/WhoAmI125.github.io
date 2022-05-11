8525c58dafc637669038ee7cd5fbcb330c7011a6
Hajong Kim <87934953+WhoAmI125@users.noreply.github.com>
2022-05-11 23:28:14 +0900

    Upload fix and clean up the repository

---
+++ source/contactform.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$text = $_POST['Subject'];
+
+$mailTo = "hajongawe@gmail.com";
+$headers = "From: ".$email;
+$txt = "You have received an e-mail from ".$name."\n\n".$text;
+mail($mailTo, $headers, $txt) or die("Error!");
+header("Location:index.html");
+
+?>
